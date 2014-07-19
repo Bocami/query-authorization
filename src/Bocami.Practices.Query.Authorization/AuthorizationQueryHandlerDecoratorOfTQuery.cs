70d121953e66b746a87cbf051696f70575c23a9e
Jonatan Pedersen <jp@jonatanpedersen.com>
2014-07-19 14:38:46 +0200

    Made it not abstract

--- src/Bocami.Practices.Query.Authorization/AuthorizationQueryHandlerDecoratorOfTQuery.cs
+++ src/Bocami.Practices.Query.Authorization/AuthorizationQueryHandlerDecoratorOfTQuery.cs
@@ -3,14 +3,14 @@ using Bocami.Practices.Decorator;
 
 namespace Bocami.Practices.Query.Authorization
 {
-    public abstract class AuthorizationQueryHandlerDecorator<TQuery, TQueryResult> : IQueryHandler<TQuery, TQueryResult>, IDecorator<IQueryHandler<TQuery, TQueryResult>>
+    public class AuthorizationQueryHandlerDecorator<TQuery, TQueryResult> : IQueryHandler<TQuery, TQueryResult>, IDecorator<IQueryHandler<TQuery, TQueryResult>>
         where TQuery : class, IQuery
         where TQueryResult : class, IQueryResult
     {
         private readonly IQueryHandler<TQuery, TQueryResult> queryHandler;
         private readonly IQueryAuthorizer<TQuery> queryAuthorizer;
 
-        protected AuthorizationQueryHandlerDecorator(IQueryHandler<TQuery, TQueryResult> queryHandler, IQueryAuthorizer<TQuery> queryAuthorizer)
+        public AuthorizationQueryHandlerDecorator(IQueryHandler<TQuery, TQueryResult> queryHandler, IQueryAuthorizer<TQuery> queryAuthorizer)
         {
             if (queryHandler == null)
                 throw new ArgumentNullException("queryHandler");
